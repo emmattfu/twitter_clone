32585489634c4591201c5a95a47a81534cd6f819
emmattfu <makcimdjan@gmail.com>
2020-10-11 22:27:41 +0300

    123

--- src/components/HomeLeft.tsx
+++ src/components/HomeLeft.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    homeLeftList: {
+      color: 'white',
+      fontWeight: 600,
+      fontSize: 20
+    },
+    leftPart: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "#71C9F8",
+      height: "100vh"
+    },
+  }),
+);
 
 const HomeLeft = () => {
+  const classes = useStyles()
+
   return (
     <>
       <Grid item xs={12} md={6}>
-        <div className="left-part" style={{backgroundColor: "#71C9F8", height: "100vh"}}>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+        <div className={classes.leftPart}>
+          <ul className={classes.homeLeftList}>
+            <li>Читайте о том, что вам интересно.</li>
+            <li>Узнайте, о чем говорят в мире.</li>
+            <li>Присоединяйтесь к общению.</li>
           </ul>
         </div>
       </Grid>
